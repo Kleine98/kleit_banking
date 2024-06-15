c355bf51a17e63a901740ddd16cc7872ac1d595f
kleine <ppyyde02@gmail.com>
2024-06-16 01:37:50 +0700

    Complete auth feature

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,7 +22,7 @@ import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-//import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -61,17 +61,17 @@ const AuthForm = ({ type }: { type: string }) => {
           password: data.password,
         };
 
-        // const newUser = await signUp(userData);
+        const newUser = await signUp(userData);
 
-        // setUser(newUser);
+        setUser(newUser);
       }
 
       if (type === "sign-in") {
-        //   const response = await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // });
-        // if (response) router.push("/");
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) router.push("/");
       }
     } catch (error) {
       console.log(error);
